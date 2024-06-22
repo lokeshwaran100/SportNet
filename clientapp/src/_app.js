a957715330bfc4a6af669cd212043fbcd94d6acb
Pratik <pratiksardar@gmail.com>
2024-06-22 20:33:10 +0530

    changing fonts

---
+++ clientapp/src/_app.js
@@ -0,0 +1,19 @@
+// _app.js
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Droid+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
