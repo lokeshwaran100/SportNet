91890e4631941911b8baf1fb7a5f38f8961eea78
Pratik <pratiksardar@gmail.com>
2024-06-23 15:46:00 +0530

    updated layout for footer at bottom

--- clientapp/src/app/layout.tsx
+++ clientapp/src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
             <UserContextProvider>
               <OwnerContextProvider>
                 <Navbar />
-                {children}
+                <main className="flex-grow">
+                  {children}
+                </main>
                 <Footer />
               </OwnerContextProvider>
             </UserContextProvider>
